8cfd03e26197d15fc4214f378a590f585be38ab8
pawan <pawangrover904@gmail.com>
2026-02-21 21:46:34 +0530

    today 10 question

---
+++ main.js
@@ -0,0 +1,48 @@
+// 1.	Add "mango" at the end of fruits.
+// let fruits=["mango"];
+// fruits.push("orange");
+// console.log(fruits);
+// 2.	Remove last element from nums.
+// let nums=[10,20,30,40,50];
+// nums.pop(50);
+// console.log(nums);
+
+// 3.	Add 0 at start of arr.
+// let arr=[1,2,3,4,5];
+// arr.unshift(0)
+// console.log(arr);
+// 4.	Remove first element from data.
+// let data =["pawan", "grover"];
+// data.shift("pawan")
+// console.log(data);
+// 5.	Print length of colors.
+// let colors ="colors";
+// // let arr=colors.length
+// console.log(colors.length);
+// 6.	Check if "cats" exists in array.
+// let animels=[ "dogs", "cats", "cows",  "horses", " lions"," tigers", "elephants"," giraffes","monkeys"]
+//   let check=animels.includes("cats");
+// console.log(check);
+
+// 7.	Join array using comma ,.
+// let animels=[ "dogs", "cats", "cows",  "horses", " lions"," tigers", "elephants"," giraffes","monkeys"]
+// let join=animels.join(",")
+// console.log(join);
+// 8.	Merge a and b.
+// let a =["pawan"];
+// let b=["grover"]
+// let arr=a.concat(b)
+// console.log(arr);
+// 9.	Convert "HELLO" into array.
+// let str="hello";
+// let newstr=Array.from(str)
+// console.log(newstr);
+// 10.	Sort numbers [3,1,2] correctly.
+// let num= [3,1,2];
+// num.sort()
+// console.log(num);
+// 11.	Find first value > 50.
+// 12.	Find index of "pen".
+// 13.	Slice first 3 elements.
+// 14.	Remove 1 element from index 0.
+// 15.	Add "A" at index 1 using splice.
